aaac8ea3c31c2243188bcc94837ad1194ccd2222
hanling3 <hedyyang1004@gmail.com>
2023-06-03 18:40:32 -0400

    Improve coding standard compliance

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,12 @@
+''' Translate text'''
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    #write the code here
-    frenchText = MyMemoryTranslator(source='auto', target='french').translate(englishText)
-    return frenchText 
+def english_to_french(english_text):
+    ''' Translate English text to French'''
+    french_text = MyMemoryTranslator(source='auto', target='french').translate(english_text)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = MyMemoryTranslator(source='auto', target='english').translate(frenchText)
-    return englishText
+def french_to_english(french_text):
+    ''' Translate French text to English'''
+    english_text = MyMemoryTranslator(source='auto', target='english').translate(french_text)
+    return english_text
